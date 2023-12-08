80203aab5b0173e3d028749145b9215ebc2c70ee
Ivan Moruhyi <53657952+imorugiy@users.noreply.github.com>
2023-12-07 22:11:18 -0500

    tests modules added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -10,11 +8,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
