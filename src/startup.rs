3cd276522d60b39063959aa96cae3ebcddfed745
Ivan Moruhyi <53657952+imorugiy@users.noreply.github.com>
2023-12-18 21:33:40 -0500

    /newsletters post & tests

--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health, subscribe};
+use crate::routes::{confirm, health, publish_newsletter, subscribe};
 
 pub struct ApplicationBaseUrl(pub String);
 
@@ -27,6 +27,7 @@ pub fn run(
             .route("/health_check", web::get().to(health))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
